a8120dc0631b84285de9b0cc8f30c381801326a6
neda-pro <neda.pro@outlook.com>
2023-12-14 14:43:10 +0100

    header section styling completed

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,7 +1,24 @@
+import AppLogo from "../assets/images/logo.svg?react";
+
 const Home = () => {
   return (
     <>
-      <h1>home</h1>
+      <header className="header">
+        <nav className="header-left">
+          <AppLogo />
+          <ul>
+            <li>feature</li>
+            <li>company</li>
+            <li>careers</li>
+            <li>about</li>
+          </ul>
+        </nav>
+        <div className="header-right">
+          <button>login</button>
+          <button>register</button>
+        </div>
+      </header>
+      <main className="main"></main>
     </>
   );
 };
