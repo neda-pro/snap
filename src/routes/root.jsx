c0827e219376d8dc81b24eb7c48633158230799e
neda-pro <neda.pro@outlook.com>
2023-12-16 22:56:13 +0100

    slider modal added

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,7 +2,7 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { useNavbarStore } from "../store/store";
 import AppLogo from "../components/AppLogo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 
 const Root = () => {
   const { hideLoginBtn, hideRegisterBtn } = useNavbarStore();
@@ -43,6 +43,35 @@ const Root = () => {
         <AppLogo />
         <FontAwesomeIcon className="bars-icon" icon={faBars} />
       </header>
+      <div className="modal">
+        <div>
+          <FontAwesomeIcon className="menu-close" icon={faClose} />
+          <ul>
+            <li>feature</li>
+            <li>company</li>
+            <li>careers</li>
+            <li>about</li>
+          </ul>
+          <div className="mobile-menu-btn-wrapper">
+            <button
+              hidden={hideLoginBtn}
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              login
+            </button>
+            <button
+              hidden={hideRegisterBtn}
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              register
+            </button>
+          </div>
+        </div>
+      </div>
       <Outlet />
     </div>
   );
